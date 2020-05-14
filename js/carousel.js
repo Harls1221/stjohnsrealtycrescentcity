43cbfb5911fecf69330b0fc7e761febd1807ea71
Timothy <t.wojtylak@gmail.com>
2020-05-13 22:55:07 -0400

    - Adding Flickity for carousel - Added 2 more images to carousel - Set images height to be 100% of parent, so height is uniform across images

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+// document.onload = function() {
+//     var flickety = new Flickety('.carousel', {
+//
+//     });
+// };
+
+var elem = document.querySelector('.carousel');
+var flickity = new Flickity(elem, {
+
+});
